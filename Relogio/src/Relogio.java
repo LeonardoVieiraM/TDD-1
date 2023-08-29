49f28594db8b8371bc868143f667adb7a564f873
LeonardoVieiraM <leonardovieiramachado@gmail.com>
2023-08-29 15:10:11 -0300

    cronometro

--- Relogio/src/Relogio.java
+++ Relogio/src/Relogio.java
@@ -18,4 +18,16 @@ public class Relogio {
         minuto = 0;
         segundo = 0;
     }
+
+    public long cronometro (int x, int y){
+        long tempo = System.currentTimeMillis();
+        long tempoInicio = System.currentTimeMillis() + x; 
+        long tempoFim = System.currentTimeMillis() + y; 
+        do{
+            tempo = System.currentTimeMillis();
+            if (tempo >= tempoInicio)
+                System.out.println(tempo-tempoInicio);
+        } while (tempo<=tempoFim);
+        return tempo;
+    }
 }
